0ef9e3aaab95195c8b2c2fb979f3eeda88bf15c3
jason93s <jason1993cortes@hotmail.com>
2022-09-03 13:57:16 -0500

    update intro-gitREADME.md and refactor intro-git/HolaMundo.java

--- intro-git/HolaMundo.java
+++ intro-git/HolaMundo.java
@@ -1,5 +1,8 @@
 class HolaMundo {
 public static void main ( String [] args ) {
-
+    saludar("jeison");
+    }
+    public static void saludar (String nombre) {
+        System.out.println("!hola" +nombre+ "!");
     }
 }
